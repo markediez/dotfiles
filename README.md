e010dc0e1b7b178378c125d91492238d5fe80dfa
Mark Diez <markediez@gmail.com>
2020-02-17 11:54:05 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# dotfiles
+
+Common setup for ubuntu / mac(?)
+
+## One-line install command
+```
+
+```
---
+++ install.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+set -xe
+
+function download_repository() {
+	echo "Downloading repository"
+
+}
+
+function install_aptget_tools() {
+	echo "Downloading core dependencies"
+	sudo apt-get update
+	echo "Installing $@"
+	sudo apt-get install -y $@
+}
+
+function install_golang() {
+	echo "Installing golang"
+	local GOLANG_HTTP='https://dl.google.com/go/go1.13.8.linux-amd64.tar.gz'
+	local GOLANG_LOC=/usr/local
+	local GOLANG_DL_LOC=/tmp/go1.13.8.linux-amd64.tar.gz
+	curl $GOLANG_HTTP -o $GOLANG_DL_LOC
+	sudo tar -C $GOLANG_LOC -xzf $GOLANG_DL_LOC
+	rm -f $GOLANG_DL_LOC
+	for binary in $(ls $GOLANG_LOC/go/bin)
+	do
+		sudo ln -s $GOLANG_LOC/go/bin/$binary /usr/local/bin/$binary
+	done
+}
+
+function install_ohmyzsh() {
+	sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+}
+
+function setup_git() {
+	echo "Setting up core config"
+	git config --global user.name "Mark Diez"
+	git config --global user.email "markediez@gmail.com"
+}
+
+function setup_zsh() {
+	ln -s $1 ~/.zsh_profile
+}
+
+function in_dotfiles () {
+	local REMOTE_URL=$(git remote get-url origin)
+	if [[ "$REMOTE_URL" == "git@github.com:markediez/dotfiles.git" ]]
+	then
+		return 0
+	else
+		return 1
+	fi
+}
+
+function main() {
+	local REPO_DIR=$(pwd)
+	# One-liner bash command invocation
+	if in_dotfiles
+	then
+		echo "Skipping download, already in dotfiles"
+	else
+		download_repository
+		REPO_DIR="$ZSH_PROFILE/dotfiles"
+	fi
+
+	install_aptget_tools gcc make git vim curl zsh
+	install_golang
+	install_ohmyzsh
+	setup_git
+	setup_zsh $REPO_DIR/.zsh_profile
+}
+
+main $@
